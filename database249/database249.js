a1f82cac575e5408f561d7c8d2306df09ecfd088
Mary Ruth Ngo <mngo@wellesley.edu>
2015-05-04 18:15:06 -0400

    Add generic tasks, random

--- database249/database249.js
+++ database249/database249.js
@@ -1,5 +1,6 @@
 
 TasksList = new Mongo.Collection("tasks");
+GenTasksList = new Mongo.Collection("genTasks");
 
 var tasksArray = [
   {text: "Tried a dining hall I don’t normally go to", category: "Eat" }, {text: "Walk around lake", category: "Health" },
@@ -8,19 +9,23 @@ var tasksArray = [
   {text: "Sandwich @ El Table", category: "Eat" }, {text: "Go to professor’s office hours", category: "Study" },
   {text: "Find 5th floor bathroom in Science Center", category: "Study" }, {text: "Go to open observatory hours", category: "Play" },
   {text: "Go to cultural event", category: "Play",}, {text: "Go to Galen Stone Tower", category: "" },
-  {text: "Study in Schneider", category: "Study" }, {text: "Study in a new part of Clapp", category: "Study" }, {text: "Study in the Lulu", category: "Study"},
+  {text: "Study in Schneider", category: "Study" }, {text: "Study in a new part of Clapp", category: "Study" }, 
+  {text: "Study in the Lulu", category: "Study"},
   {text: "Play billiards on Lulu 3rd floor", category: "Play"},  {text: "Check out a projector and have a movie night!", category: "Social" },
   {text: "Check out a book for fun from library", category: "Play" }, {text: "Study in Billings", category: "Study" },
   {text: "Go to Bingo night!", category: "Play" }, {text: "Go canoeing/kayaking on lake", category: "Play" },
-  {text: "Watch a Varsity game - Have some Wellesley Blue spirit!", category: "Play"},  {text: "Watch a club sport game ", category: "Play"},  {text: "Watch an acapella performance", category: "Play"}, 
-  {text: "Go to an org’s open meeting", category: "Play"},  {text: "Look at student art in Jewett", category: "Play"}, 
-  {text: "Go to Davis Museum", category: "Play"}, {text: "Try out sauna at KSC!", category: "Health"},  {text: "Intramural sport game!", category: "Health"}, 
+  {text: "Watch a Varsity game - Have some Wellesley Blue spirit!", category: "Play"},  {text: "Watch a club sport game ", category: "Play"},  
+  {text: "Watch an acapella performance", category: "Play"}, {text: "Go to an org’s open meeting", category: "Play"},  
+  {text: "Look at student art in Jewett", category: "Play"}, 
+  {text: "Go to Davis Museum", category: "Play"}, {text: "Try out sauna at KSC!", category: "Health"},  
+  {text: "Intramural sport game!", category: "Health"}, 
   {text: "Go to special collections in Clapp", category: "Play"},  {text: "Go rock climbing at KSC", category: "Health"}, 
-  {text: "Check something fun out at KSC", category: "Health"},  {text: "Go to a Wellesley Theatre performance", category: "Play"}, 
+  {text: "Find out for yourself whether there's a sauna at KSC", category: "Health"},  {text: "Go to a Wellesley Theatre performance", category: "Play"}, 
   {text: "Go to a Shakes performance", category: "Play"},  {text: "Go to Natick Mall", category: "Play"}, 
-  {text: "Furniture Store iMAX in Natick!!?", category: "Play"},  {text: "Go to an off-campus event!", category: "Play"}, 
-  {text: "Get a sandwich from Lulu - get the avocado on Fridays!", category: "Eat"},  {text: "Look for woodland creatures during your walk through campus", category: "Play"}, 
-  {text: "Check something off one of the 50 things", category: "Play"},  {text: "Study in Shafer living room", category: "Study"}, 
+  {text: "Watch a movie at the super cool furniture Store iMAX in Natick!!?", category: "Play"},  {text: "Go to an off-campus event!", category: "Play"}, 
+  {text: "Take advantage of Avocado Friday at Lulu", category: "Eat"},  
+  {text: "Look for woodland creatures during your walk through campus", category: "Play"}, 
+  {text: "Check something off one of the 50 things to do before you graduate", category: "Play"},  {text: "Study in Shafer living room", category: "Study"}, 
   {text: "Study in Pom living room", category: "Study"},  {text: "Study in Beebe living room", category: "Study"}, 
   {text: "Study in Caz living room", category: "Study"},  {text: "Invite friends over to dorm room", category: "Social"}, 
   {text: "Host a baking party", category: "Social"},  {text: "Use your dorm kitchen", category: "Eat"}, 
@@ -31,17 +36,27 @@ var tasksArray = [
   {text: "Study in Severance", category: "Study"},  {text: "Study in Bates", category: "Study"}, 
   {text: "Study in Freeman", category: "Study"},  {text: "Study in McAfee", category: "Study"}, 
   {text: "Study in Dower", category: "Study"},  {text: "Talk to a BHE", category: "Health"}, 
-  {text: "Talk to APT", category: "Study"},  {text: "Talk to HP", category: "Health"}, 
-  {text: "Talk to MHE", category: "Health"},  {text: "Visit your major advisor!", category: "Social"}, 
+  {text: "Talk to an MHE", category: "Health"},  {text: "Visit your major advisor!", category: "Social"}, 
   {text: "Visit your dean", category: "Social"},  {text: "Check out the taxidermy in Science Center", category: "Play"}, 
   {text: "Look at the glow in the dark rocks by geos", category: "Play"},  {text: "Visit student lounge of your major", category: "Play"}, 
   {text: "Visit your dean", category: "Study"},  {text: "Play with the water table", category: "Play"}, 
-  {text: "Go to Leaky Beaker", category: "Eat"},  {text: "Puzzle table in Sci Library", category: "Play"}, 
+  {text: "Get some Peet's coffe at the Leaky Beaker", category: "Eat"},  {text: "Puzzle table in Sci Library", category: "Play"}, 
   {text: "Study in Art Library", category: "Study"},  {text: "Study in Music Library", category: "Study"}, 
   {text: "Go to a floor/hall program", category: "Social"},  {text: "Watch Mona Lisa Smile", category: "Play"}, 
   {text: "Host a Prospie", category: "Social"},  {text: "Buy something on Free and For Sale", category: "Play"}, 
-  {text: "Sell something on Free and For Sale", category: "Play"},  {text: "Therapy Dogs!", category: "Health"}, 
+  {text: "Post something on Free and For Sale", category: "Play"},  {text: "Therapy Dogs!", category: "Health"}, 
   {text: "Study in Sage Lounge Science Center", category: "Study"}];
+
+var genArray = [
+  {text: "Took a nap", category: "Sleep", points: 3}, {text: "Hung out with friends", category: "Social", points: 5},
+  {text: "Called home, whatever that may be to you", category: "Social", points: 6}, {text: "Left campus!", category: "Social", points: 10},
+  {text: "Finished homework before midnight", category: "Study", points: 3}, {text: "Studied for Test", category: "Study", points: 3},
+  {text: "Participated in class", category: "Study", points: 2}, {text: "Went to class", category: "Study", points: 1},
+  {text: "Ate all 3 meals!", category: "Health", points: 8},
+  {text: "Exercised", category: "Health", points: 10}, {text: "Took a shower", category: "Health", points: 1},
+  {text: "Took medication (if needed)", category: "Health", points: 1}, {text: "Brushed teeth twice today", category: "Health", points: 2},
+  {text: "Read a book for fun", category: "Play", points: 5}, {text: "Listened to music", category: "Play", points: 2},
+  {text: "Went outside!", category: "Play", points: 5}];
 
 if(Meteor.isClient) {
 
@@ -49,8 +64,6 @@ if(Meteor.isClient) {
     Template.allTasks.helpers({
 
       'userPoints': function() {
-        //points as property in user collection object
-        //return this.points;
         return 5;
       },
 
@@ -78,36 +91,85 @@ if(Meteor.isClient) {
         return TasksList.find({category: "Sleep"}).count()
       },
 
+      'counterGenTasks': function() {
+        return GenTasksList.find().count()
+      },
+
+      'genTask': function() {
+        return GenTasksList.find();
+      },
+
+      'randomTasks': function() {
+        var i = Math.floor(Math.random() * TasksList.find().count());
+        var j = Math.floor(Math.random() * TasksList.find().count());
+        var k = Math.floor(Math.random() * TasksList.find().count());
+        var first = TasksList.find({text: tasksArray[i].text}).fetch()[0].text;
+        var second = TasksList.find({text: tasksArray[j].text}).fetch()[0].text;
+        var third = TasksList.find({text: tasksArray[k].text}).fetch()[0].text;
+        return [first, " " + second, " " + third];
+      }
+
 
     });
 }
 
 if(Meteor.isServer){
 
-  if (TasksList.find().count() == 0) {
-    Meteor.call('insertTaskData');
+  if (TasksList.find().count() > 0) {
+    TasksList.remove({});
+  }
+  for (i in tasksArray) {
+    console.log("inside for loop");
+    TasksList.insert({
+      text: tasksArray[i].text,
+      points: 15,
+      category: tasksArray[i].category
+    });
+  }
+
+  if (GenTasksList.find().count() > 0) {
+    GenTasksList.remove({});
+  }      
+  for (i in genArray) {
+    GenTasksList.insert({
+      content: genArray[i].text,
+      points: genArray[i].points,
+      category: genArray[i].category
+    });
   }
 
   Meteor.methods({
-
     //method
     'insertTaskData': function(){
       for (i in tasksArray) {
         console.log("inside for loop");
-          TasksList.insert({
-            text: tasksArray[i].text,
-            points: 15,
-            category: tasksArray[i].category
-          });
+        TasksList.insert({
+          text: tasksArray[i].text,
+          points: 15,
+          category: tasksArray[i].category
+        });
+      }
+    },
+
+    'insertGenericData': function(){
+      for (i in genArray) {
+        GenTasksList.insert({
+          content: genArray[i].text,
+          points: genArray[i].points,
+          category: genArray[i].category
+        });
       }
     },
 
     //another method
     'removeAllTasks': function() {
-        return TasksList.remove({});
+      return TasksList.remove({});
     },
 
-    //another method
+    'removeAllGenTasks': function() {
+      return GenTasksList.remove({});
+    }
+    
   });
 
 }
