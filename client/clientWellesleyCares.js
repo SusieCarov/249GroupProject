2d4e879c10a5da98bdb322610f2e201ac87d7af6
clareblue79 <clee19@wellesley.edu>
2015-05-12 17:49:57 -0400

    Deleting Uneccessary

--- client/clientWellesleyCares.js
+++ client/clientWellesleyCares.js
@@ -31,4 +31,69 @@ TasksList = new Mongo.Collection("tasks");
        // TasksList.update(this._id, {$set: {skipped: true}});
       }
     });
+
+    Template.body.helpers({
+        
+        firstName: function(){
+            var user = Meteor.user(); 
+            if (user) {
+                return user.services.google.given_name;    
+            } 
+        },
+   
+        profileURL: function() {
+            var user = Meteor.user(); 
+            if (user) {
+                return user.services.google.picture; 
+            } 
+        },
+        
+        initializePoints: function(){
+            console.log("first time user, updating fields")
+            Meteor.users.update({_id:Meteor.user()._id}, {$set:{profile: {playPoints: 0, studyPoints: 0, socialPoints: 0, sleepPoints:0,            healthPoints:0}}});
+        },
+        
+        pointsImplemented: function(){
+            console.log("You have already initialized the points");
+        },
+        
+        userhaspoints: function(){
+            var you = Meteor.user().profile;
+            console.log(you);
+            if (you.hasOwnProperty("playPoints")){
+                return true;
+                } else{
+                return false;
+                }
+        },
+        
+         myPoints: function() {
+            return Meteor.user().profile;
+        }
+        
+});
+
+    Template.points.helpers({
+        pointInfo: function() {
+            return Meteor.users.find({}, {sort:{profile: {playPoints: -1, studyPoints: -1, socialPoints: -1, sleepPoints: -1, healthPoints: -1, name: 1}}});
+        },
+        users: function(){
+            
+            return Meteor.users.find({}, {"services.google.name": 1, 
+                             "services.google.picture": 1,
+                             "services.google.email" : 1});
+        },
+    
+        username: function(){
+            return this.services.google.name;
+        },
+    
+        photoURL: function(){
+            return this.services.google.picture;
+        }
+
+        
+                                      
+    });
+    
   
